8f9ae10e71194f5cf3a8d9033d842fd4f3f042a8
kai-tooley <kai.p.o.tooley@gmail.com>
2019-10-28 12:25:23 +0000

    object type enum

---
+++ Overcoded/Assets/Scripts/ObjectTypes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTypes : MonoBehaviour
+{
+    public enum ObjectType
+    {
+        RAW_SHAPES,
+        PROCESSED_SHAPES,
+        RAW_CODE,
+        PROCESSED_CODE,
+        RAW_AUDIO,
+        PROCESSED_AUDIO,
+        RAW_CONCEPTS,
+        PROCESSED_CONCEPTS
+    }
+}
