7560e4224cdbec6e8638e5e80c4f143d11afc3cb
kai-tooley <kai.p.o.tooley@gmail.com>
2019-10-29 13:56:15 +0000

    merge changes

--- Overcoded/Assets/Scripts/PlayerController.cs
+++ Overcoded/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,10 +16,12 @@ public class PlayerController : MonoBehaviour
 
     NavMeshAgent agent;
     Rigidbody rb;
+    public bool canMove;
     public SpriteRenderer spriteRenderer; //This has to be manually set as doing it via GetComponentInChildren gets the player's spriterenderer
+
+
     ObjectType resourceType = ObjectType.NO_RESOURCE;
     public float resourceProgress;
-
 
     public Material progressNone;
     public Material progressGold;
@@ -34,7 +35,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && canMove)
         {
             RaycastHit hit;
 
@@ -66,49 +67,12 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
-
-/// <summary>
-/// Player Controller Script, that currently contains:
-/// * A simple click to move function
-/// </summary>
-
-
-
-public class PlayerController : MonoBehaviour
-{
-    public Vector3 velocity;
-
-    NavMeshAgent agent;
-    Rigidbody rb;
-    public SpriteRenderer spriteRenderer; //This has to be manually set as doing it via GetComponentInChildren gets the player's spriterenderer
-    public ObjectType resourceType;
-    public bool canMove;
-
-    void Start()
+    public void ClearHeldObject()
     {
-        agent = GetComponent<NavMeshAgent>();
-        rb = GetComponent<Rigidbody>();
-        resourceType = ObjectType.NO_RESOURCE;
         spriteRenderer.sprite = null;
-    }
+        resourceType = ObjectType.NO_RESOURCE;
 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0) && canMove)
-        {
-            RaycastHit hit;
-
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
-            {
-                agent.destination = hit.point;
-            }
-
-        }
+        resourceProgress = 0;
     }
 
     void OnTriggerEnter(Collider other)
@@ -125,22 +89,9 @@ public class PlayerController : MonoBehaviour
         StartCoroutine(countdown());
     }
 
->>>>>>> develop
-    public void ClearHeldObject()
-    {
-        spriteRenderer.sprite = null;
-        resourceType = ObjectType.NO_RESOURCE;
-<<<<<<< HEAD
-        resourceProgress = 0;
-    }
-}
-=======
-    }
-
     IEnumerator countdown() 
     {
         yield return new WaitForSeconds(5);
         canMove = false;
     }
 }
->>>>>>> develop
