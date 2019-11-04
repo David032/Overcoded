b8e8eeb37716b9eac0b2ca0e06603f3f5f6e51e0
Jake <pokepeel15@yahoo.co.uk>
2019-11-04 18:43:26 +0000

    Added the Timer script and Display of current in-game time

--- Overcoded/Assets/Scripts/In Game/GetTime.cs
+++ Overcoded/Assets/Scripts/In Game/GetTime.cs
@@ -2,25 +2,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GetTime : MonoBehaviour
 {
     GameObject manager;
-    FeatureGeneration managerGenerator;
-    FeatureGeneration generationSystem;
-
+    public Text timer_text;
+    float game_time;
 
 
 
     void Start()
     {
         manager = GameObject.FindGameObjectWithTag("GameController");
-        managerGenerator = manager.GetComponent<FeatureGeneration>();
-        
+        game_time = manager.GetComponent<GameController>().GameTime;
     }
 
     void Update()
     {
-        
+        game_time -= Time.deltaTime;
+        int game_timer_int = Mathf.CeilToInt(game_time);
+        int game_min, game_sec;
+        game_min = (game_timer_int / 60);
+        game_sec = (game_timer_int % 60);
+        timer_text.text = game_min.ToString() + ":" + game_sec.ToString();
     }
 }
