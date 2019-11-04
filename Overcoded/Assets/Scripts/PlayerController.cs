994191e4b2fadfea74ed31b6ec32f8a6e02a6c5d
David Kelly <davek032@outlook.com>
2019-11-04 16:02:31 +0000

    Random Player Set-up

--- Overcoded/Assets/Scripts/PlayerController.cs
+++ Overcoded/Assets/Scripts/PlayerController.cs
@@ -18,7 +18,8 @@ public class PlayerController : MonoBehaviour
     Rigidbody rb;
     AudioController audio;
     public bool canMove;
-    public SpriteRenderer spriteRenderer; //This has to be manually set as doing it via GetComponentInChildren gets the player's spriterenderer
+    public SpriteRenderer iconRenderer; //This has to be manually set as doing it via GetComponentInChildren gets the player's spriterenderer
+    SpriteRenderer playerRenderer;
 
 
     public ObjectType resourceType = ObjectType.NO_RESOURCE; 
@@ -30,18 +31,38 @@ public class PlayerController : MonoBehaviour
 
     public bool isHolding = false;
 
+    public Sprite APlayer;
+    bool aPlayer;
+    public Sprite APLayerAlt;
+    public Sprite BPlayer;
+    bool bPlayer;
+    public Sprite BPLayerAlt;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         rb = GetComponent<Rigidbody>();
         audio = GetComponent<AudioController>();
+
+        int rndNumber = Random.Range(0, 2);
+        if (rndNumber == 0)
+        {
+            playerRenderer.sprite = APlayer;
+            aPlayer = true;
+        }
+        if (rndNumber == 1)
+        {
+            playerRenderer.sprite = BPlayer;
+            bPlayer = true;
+        }
     }
 
     public bool amHolding() { return isHolding; }
 
     void Update()
     {
+        spriteChange();
+
         if (Input.GetMouseButtonDown(0) && canMove)
         {
             RaycastHit hit;
@@ -54,21 +75,41 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void spriteChange()
+    {
+        if (resourceType != ObjectType.NO_RESOURCE && aPlayer)
+        {
+            playerRenderer.sprite = APLayerAlt;
+        }
+        else if (resourceType == ObjectType.NO_RESOURCE && aPlayer)
+        {
+            playerRenderer.sprite = APlayer;
+        }
+        if (resourceType != ObjectType.NO_RESOURCE && bPlayer)
+        {
+            playerRenderer.sprite = BPLayerAlt;
+        }
+        else if (resourceType == ObjectType.NO_RESOURCE && bPlayer)
+        {
+            playerRenderer.sprite = BPlayer;
+        }
+    }
+
     public void PickUpObject(Sprite sprite, ObjectType objectType, float progress = 0)
     {
         if (this.resourceType == ObjectType.NO_RESOURCE && !isHolding)
         {
-            spriteRenderer.sprite = sprite;
+            iconRenderer.sprite = sprite;
             resourceType = objectType;
             resourceProgress = progress;
 
             if (progress < 1.0f)
             {
-                spriteRenderer.material.Lerp(progressNone, progressGold, progress);
+                iconRenderer.material.Lerp(progressNone, progressGold, progress);
             }
             else
             {
-                spriteRenderer.material.Lerp(progressGold, progressMud, progress - 1);
+                iconRenderer.material.Lerp(progressGold, progressMud, progress - 1);
             }
             isHolding = true;
         }
@@ -81,8 +122,8 @@ public class PlayerController : MonoBehaviour
         {
             GameObject resource = new GameObject();
             resource.AddComponent<SpriteRenderer>();
-            resource.GetComponent<SpriteRenderer>().sprite = spriteRenderer.sprite;
-            resource.GetComponent<SpriteRenderer>().material = spriteRenderer.material;
+            resource.GetComponent<SpriteRenderer>().sprite = iconRenderer.sprite;
+            resource.GetComponent<SpriteRenderer>().material = iconRenderer.material;
 
             resource.AddComponent<ResourceState>();
             resource.GetComponent<ResourceState>().Set(resourceType, resourceProgress);
@@ -99,7 +140,7 @@ public class PlayerController : MonoBehaviour
 
     public void ClearHeldObject()
     {
-        spriteRenderer.sprite = null;
+        iconRenderer.sprite = null;
         resourceType = ObjectType.NO_RESOURCE;
         isHolding = false;
         resourceProgress = 0;
@@ -109,7 +150,7 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.tag == "Resource")
         {
-            spriteRenderer.sprite = other.gameObject.GetComponentInChildren<SpriteRenderer>().sprite;
+            iconRenderer.sprite = other.gameObject.GetComponentInChildren<SpriteRenderer>().sprite;
         }
     }
 
