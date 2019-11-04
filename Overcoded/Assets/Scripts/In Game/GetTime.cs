76e55a4c9dc6484e1ad9eaa568a70baff3224c9b
Jake <pokepeel15@yahoo.co.uk>
2019-11-04 17:54:47 +0000

    Added Timer script

---
+++ Overcoded/Assets/Scripts/In Game/GetTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetTime : MonoBehaviour
+{
+    GameObject manager;
+    FeatureGeneration managerGenerator;
+    FeatureGeneration generationSystem;
+
+
+
+
+    void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("GameController");
+        managerGenerator = manager.GetComponent<FeatureGeneration>();
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
