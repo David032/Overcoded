ce8456b1b2436974c54bb2acfd1526e11a4f54dd
kai-tooley <kai.p.o.tooley@gmail.com>
2019-11-05 17:05:01 +0000

    fixed mistake with menu canvas, exit button appears to work in build

--- Overcoded/Assets/Scripts/Menu/Main Menu/MainMenu.cs
+++ Overcoded/Assets/Scripts/Menu/Main Menu/MainMenu.cs
@@ -44,7 +44,13 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Audio.PlayLevelChange();
-        Application.Quit();
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
     }
 
     public void SetLevelNumber(int level)
